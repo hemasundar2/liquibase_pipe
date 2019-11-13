db9785b9b6942312d03272bc20de8fa69b36a111
Lanka, Hemasundara [A] <hemlanka@publicisgroupe.net>
2019-11-12 20:17:28 -0600

    commit

---
+++ src/main/java/gov/hrsa/nextgen/ums/db/umsliquibase/UmsLiquibaseApplication.java
@@ -0,0 +1,13 @@
+package gov.hrsa.nextgen.ums.db.umsliquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmsLiquibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmsLiquibaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gov/hrsa/nextgen/ums/db/umsliquibase/UmsLiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.hrsa.nextgen.ums.db.umsliquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmsLiquibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
